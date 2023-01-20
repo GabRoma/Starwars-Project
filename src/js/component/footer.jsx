cfabc769f8194d7fa4f88d1d9f1d22d472c4e1f7
GabRoma <gabrielrg901@gmail.com>
2023-01-20 14:03:13 +0000

    ajustes al footer

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -3,9 +3,9 @@ import "../../styles/footer.css";
 
 export const Footer = () => (
 	<footer className="footer text-center bg-black">
-		<p className="credits">Made by{" "}
-			<a style={{color:"white"}} href="https://github.com/GabRoma">
-			<i className="fal fa-space-station-moon-alt"/>Gab Roma</a>
+		<p className="credits pt-1">Made by{" "}
+			<a style={{color:"white"}} href="https://github.com/GabRoma" target="_blank">
+			Gab Roma <i className="fas fa-rocket"/></a>
 		</p>
 	</footer>
 );
